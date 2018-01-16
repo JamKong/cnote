647561cb95b69bc63cb849c2fceb1a14fbedd088
JamKong <236704627@qq.com>
2018-01-16 22:04:48 +0800

    update

--- config/default.js
+++ config/default.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-	// imageServer: "http://47.104.101.246:3000",
 	imageServer:"http://localhost:3001",
 	wangeditUploadApi: "/api/upload/wangedit",
 	base64UploadApi: "/api/upload/img"
